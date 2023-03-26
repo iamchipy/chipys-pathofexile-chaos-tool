565ddb8c090a5bf7ca4bc6df0e1793e95f211cb4
Chipy <iamchipy@gmail.com>
2023-03-26 11:25:56 -0400

    changed info to private

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 
 import poe_oauth
-import info
+import private
 
 class StashSearching():
     pass
 
-authentication = poe_oauth.PoeApiHandler(client_id=info.CLIENT_ID,
-                                         client_secret=info.CLIENT_SECRET,
-                                         scope=info.SCOPE,
-                                         uri=info.REDIRECT_URI,
-                                        #  manual_token=info.DEV_TOKEN
+authentication = poe_oauth.PoeApiHandler(client_id=private.CLIENT_ID,
+                                         client_secret=private.CLIENT_SECRET,
+                                         scope=private.SCOPE,
+                                         uri=private.REDIRECT_URI,
+                                         manual_token=private.DEV_TOKEN
                                         )
 
 print(authentication.stash_tab_names("hardcore"))
 print(authentication.stash_tab_names())
+# print(authentication.stash_tab_names("hardcore/$"))
 
 # authenticate 
 ## check for timeout
