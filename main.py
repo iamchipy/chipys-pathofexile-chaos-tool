82792dce4b3db33ef4e281ea1901aa4267d93b90
Chipy <iamchipy@gmail.com>
2023-03-28 14:44:33 -0400

    Updates and functionality to main/auth

--- main.py
+++ main.py
@@ -1,25 +1,149 @@
 
-import poe_oauth
-import private
+import json
+from poe_oauth import PoeApiHandler
+from base_types import BASE_TYPES
+import user_info
 
-class StashSearching():
-    pass
 
-authentication = poe_oauth.PoeApiHandler(client_id=private.CLIENT_ID,
-                                         client_secret=private.CLIENT_SECRET,
-                                         scope=private.SCOPE,
-                                         uri=private.REDIRECT_URI,
-                                         manual_token=private.DEV_TOKEN
-                                        )
 
-print(authentication.stash_tab_names("hardcore"))
-print(authentication.stash_tab_names())
-# print(authentication.stash_tab_names("hardcore/$"))
+class DataParser():
+    def __init__(self, api_handler:PoeApiHandler=None, league="standard") -> None:
+        if not api_handler:
+            print("API object missing. MAKE sure to use '.new_api_handler(api_handler)'")
+        self.api_handler = api_handler
 
-# authenticate 
-## check for timeout
-# fetch stash data
-## filter stash data
-### give read out GUI
+        self.league = league
+        self.cached = {"DEFAULT_VALUE":0}
 
+    def _cache_stash(self, league:str):
+        self.cached[league+"_stash_response"] = self.api_handler.get_stash(league)
+        # print(type(self.cached[league+"_stash_response"]))
+        self.cached[league+"_stash"] = json.loads(self.cached[league+"_stash_response"].content)["stashes"]
+        # print(self.cached[league+"_stash"])
+    
+    def _parse_tab_names(self, stash:dict, filter_remove_only=True) -> dict:
+        result = [[i["name"],i["id"]] for i in stash]
+        
+        if filter_remove_only:
+            result = [i for i in result if "Remove-only" not in i[0]]
+
+        return dict(result)
+    
+    def get_tab_names(self, league="standard") -> dict:
+        if league+"_stash" not in self.cached:
+            self._cache_stash(league)
+        self.cached[league+"_tab_names"] = self._parse_tab_names(stash=self.cached[league+"_stash"])
+        return self.cached[league+"_tab_names"]
+        
+    def _cache_tab(self, league:str, stash_id:str):
+        self.cached[league+"_"+stash_id+"_response"] = self.api_handler.get_tab(league, stash_id)
+        raw=json.loads(self.cached[league+"_"+stash_id+"_response"].content)
+        # print(raw)
+        # assert "children" not in raw["stash"]  # assert not a parent/nested tab
+        self.cached[league+"_"+stash_id] = raw["stash"]["items"]
+
+    def _parse_item_names(self, tab:dict) -> list:
+        result = [[i["name"],i["id"]] for i in tab]
+        return result      
+
+    def get_item_names(self,  stash_id:str="52dc1b3814", league="hardcore") -> dict:
+        if league+"_"+stash_id not in self.cached:
+            self._cache_tab(league,stash_id)
+        self.cached[league+"_"+stash_id+"_items"] = self._parse_item_names(self.cached[league+"_"+stash_id])
+        return self.cached[league+"_"+stash_id+"_items"] 
+    
+    def get_items(self,  stash_id:str="52dc1b3814", league="hardcore") -> dict:
+        if league+"_"+stash_id not in self.cached:
+            self._cache_tab(league,stash_id)
+        self.cached[league+"_"+stash_id+"_items"] = self._parse_item_names(self.cached[league+"_"+stash_id])
+        return self.cached[league+"_"+stash_id] 
+    
+    def get_items_filtered(self,  haystack:list) -> list:
+        return [i for i in haystack if i["identified"] is False]
+
+    def _cache_characters(self):
+        self.cached["characters_response"] = self.api_handler.get_characters()
+        self.cached["characters"] = json.loads(self.cached["characters_response"].content)["characters"]
+
+    def _parse_character_names(self, characters):
+        result = [[i["name"],i["league"]] for i in characters]
+        return result      
+    
+    def get_characters(self) -> list:
+        if "characters" not in self.cached:
+            self._cache_characters()
+        return self._parse_character_names(self.cached["characters"])   
+
+    def _cache_leagues(self):
+        self.cached["leagues_response"] = self.api_handler.get_leagues()
+        self.cached["leagues"] = json.loads(self.cached["leagues_response"].content)["leagues"]
+
+    def _parse_league_names(self, characters):
+        result = [i["id"] for i in characters if i["realm"] == "pc"]
+        return result  
+
+    def get_leagues(self) -> list:
+        """Base leagues:
+            - 'Standard'
+            - 'Hardcore'
+            - 'SSF Standard'
+            - 'SSF Hardcore'
+        Returns:
+            list: List of active leagues
+        """
+        if "leagues" not in self.cached:
+            self._cache_leagues()
+        return self._parse_league_names(self.cached["leagues"] )
+
+def set_league(parser:DataParser, user_input:str=None):
+    active_leagues = parser.get_leagues()
+    if not user_input:
+        user_input = input("Select League: ").lower()
+    for league in active_leagues:
+        if user_input in str(league).lower():
+            print("League auto-corrected to:",league)
+            return league
+    return False
+
+def set_tab(parser:DataParser, user_input:str=None):
+    tab_names = parser.get_tab_names(league_of_interest)
+    if not user_input:
+        user_input = input("Select tab: ").lower()
+    for tab in tab_names.items():
+        if user_input in str(tab).lower():
+            print("League auto-corrected to:",tab)
+            return tab
+    return False
+
+def index_unid(parser:DataParser, list_of_items:list):
+    counts={"a":1}
+    for item in list_of_items:
+        slot = BASE_TYPES[item["baseType"]]
+        if slot in counts:
+            counts[slot] +=1
+        else:
+            counts[slot] = 1
+    return counts
+
+if __name__ == "__main__":
+    authentication = PoeApiHandler(client_id=user_info.cfg["api"]["CLIENT_ID"],
+                                   client_secret=user_info.cfg["api"]["CLIENT_SECRET"],
+                                   scope=user_info.cfg["api"]["SCOPE"],
+                                   uri=user_info.cfg["api"]["REDIRECT_URI"],
+                                   manual_token=user_info.cfg["api"]["DEV_TOKEN"]
+                                   )
+    parser = DataParser(api_handler = authentication)
+
+    # set league
+    league_of_interest = set_league(parser, "stand")
+
+    # tab_of_interes
+    tab_of_interest = set_tab(parser, "dt")
+
+    # filter for unid
+    list_of_items_unidentified = parser.get_items_filtered(parser.get_items(tab_of_interest[1],league_of_interest))
+    
+    # loop and count unids
+    print(index_unid(parser, list_of_items_unidentified))
+ 
 
--- poe_oauth.py
+++ poe_oauth.py
@@ -7,7 +7,7 @@ import websockets
 import webbrowser
 from urllib.parse import urlparse, parse_qs
 
-HEADER_USER_AGENT = {"User-Agent": "OAuth chipytools/0.0.1 (Contact: contact@chipy.dev)"}
+HEADER_USER_AGENT ={"User-Agent": "OAuth chipytools/0.0.1 (Contact: contact@chipy.dev)"}
 HEADER_TYPE = {"Content-Type": "application/x-www-form-urlencoded"}
 URL_AUTH = r"https://www.pathofexile.com/oauth/authorize"
 URL_TOKEN = r"https://www.pathofexile.com/oauth/token"
@@ -18,10 +18,21 @@ API_LEAGUE = API_ENDPOINT+"league"
 API_STASH = API_ENDPOINT+"stash/"
 
 DEPTH_ITEMS = 2
+DEPTH_STASH_NAMES = 1
+
+"""
+PROCESS
+- Connection handler
+- - Calls
+- - - Output data
+- Data handler
+- - processer
+- - 
+"""
 
 class PoeApiHandler():
-    def __init__(self, client_id, client_secret, uri, scope="account:stashes", force_re_auth:bool=False, manual_token:str=None):
-        print("Building variables ...")
+    def __init__(self, client_id, client_secret, uri, scope="account:profile", force_re_auth:bool=False, manual_token:str=None):
+        print("Building variables . . . ",end=" ")
         self.id = client_id
         self.secret = client_secret
         self.uri = uri
@@ -29,38 +40,44 @@ class PoeApiHandler():
         self.scope = scope
         self.code = ""
         self.token = ""
-
         self.headers = {**HEADER_TYPE,**HEADER_USER_AGENT}  
+        print("done")
+
         if manual_token:
             self._update_header_token(manual_token)
-   
-        self._authenticate(force_re_auth)
+        else:
+            self._authenticate(force_re_auth)
+        
 
     async def parse(self, url):
         print("Parsing...")
         self.url_dict = urlparse(url)
         queries = parse_qs(self.url_dict[4])  #{'code': ['28bf9a843e5626d47a125ad384fe53f6e998335f'], 'state': ['fa6437e646eabcd1a575a81a06c94a1f74abc68d16bac2414d004241233c8b76']}
+        if "error" in queries:
+            print("PROBLEM WITH OAUTH REPLY:")        
+            print(queries)
+            exit()
         self.state=queries["state"][0]
         self.code=queries["code"][0]
 
     async def echo(self, websocket, path):
         async for message in websocket:
             await websocket.send(message)
-            print("Received data:", message)
+            # print("Received data:", message)
             self.msg = message
             await self.parse(self.msg)
             self._exit.set_result(None)
                 
     async def echo_server(self):
         async with websockets.serve(self.echo, '127.0.0.1', 32111):
-            print('Awaiting authentication...')
+            print('Awaiting approval . . . ', end=" ")
             await self._exit
         print('Done')        
 
     def _update_header_token(self,token_to_update_with=False):
         if token_to_update_with:
             self.token = token_to_update_with
-        self.headers = {"Authorization": "Bearer "+self.token,
+        self.headers = {"Authorization": "Bearer "+self.token, 
                         **HEADER_USER_AGENT}
         print("NEW TOKEN:",self.token)
         
@@ -70,12 +87,13 @@ class PoeApiHandler():
         return False
 
     def _still_authenticated(self):
-        if self._success_code_200(self._get_stash_raw("standard")):
+        if self._success_code_200(self.get_stash("standard")):
             return True
         print("FAILED INIT AUTH")
         return False
             
     def _authenticate(self, force_re_auth=False):
+        print("Authenticating . . . ")
         # test status
         if self._still_authenticated() and not force_re_auth:
             return
@@ -87,15 +105,16 @@ class PoeApiHandler():
             #LOGGING
             pass 
         # Building URL for permission request
-        print("Building request ...")
+        # print("Building request ...")
         client_str = '?client_id='+self.id
         response_type = '&response_type=code'
         scope_str = '&scope='+self.scope
         state_str = '&state='+self.state
         redir = '&redirect_uri='+self.uri
 
-        print("Initializing OAuth2 ...")
+        # print("Initializing OAuth2 ...")
         webbrowser.open(URL_AUTH+client_str+response_type+scope_str+state_str+redir)
+        # print(URL_AUTH+client_str+response_type+scope_str+state_str+redir)
 
         # Start the async loop
         print("Waiting for approval ...")
@@ -131,33 +150,72 @@ class PoeApiHandler():
         self._update_header_token(self.auth_reply_raw["access_token"])
         return self._still_authenticated()
 
-    def _get_stash_raw(self, league):
-        stashes = API_STASH+league
-        return requests.get(stashes, headers=self.headers)
+    def get_stash(self, league) -> requests.Response:
+        # return json.loads(requests.get(API_STASH+league, headers=self.headers).content)
+        return requests.get(API_STASH+league, headers=self.headers)
     
-    def get_stash(self, league="standard") -> dict:
-        return json.loads(self._get_stash_raw(league).content)
+    def get_profile(self) -> requests.Response:
+        # return json.loads(requests.get(API_PROFILE, headers=self.headers).content)
+        return requests.get(API_PROFILE, headers=self.headers)
+    
+    def get_tab(self, league:str, stash_id:str) -> requests.Response:
+        return requests.get(API_STASH+league+"/"+stash_id, headers=self.headers)
+    
+    def get_leagues(self) -> requests.Response:
+        # this is a service scope request so it's a bit different (not private so no auth needed)
+        #https://www.pathofexile.com/developer/docs/authorization#client_credentials
+        # first get a token
+        data = {"client_id":self.id,
+                "client_secret": self.secret,
+                "grant_type":"client_credentials",
+                "scope":"service:leagues"}
+        r = requests.post(URL_TOKEN, data=data, headers={**HEADER_TYPE,**HEADER_USER_AGENT} )
+        # adjust a temp header
+        temp_header = {**HEADER_TYPE,
+                       **HEADER_USER_AGENT,
+                       "Authorization":"Bearer "+json.loads(r.content)["access_token"]}
+        return requests.get(API_LEAGUE, headers=temp_header)    
+    
+    def get_characters(self) -> requests.Response:
+        return requests.get(API_CHARACTER, headers=self.headers)    
 
-    def _get_profile_raw(self):
-        return requests.get(API_CHARACTER, headers=self.headers)
+# {'verified': False, 
+#  'w': 1, 
+#  'h': 1, 
+#  'icon': 'https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvR2Vtcy9TdXBwb3J0L0luY3JlYXNlZFF1YW50aXR5IiwidyI6MSwiaCI6MSwic2NhbGUiOjF9XQ/306f059231/IncreasedQuantity.png', 
+#  'support': True, 
+#  'league': 'Standard', 
+#  'id': '03fbe5019da2870ebab84aaeb7c8c2e6402b36a7547432164204be756832612d', 
+#  'name': '', 
+#  'typeLine': 'Item Quantity Support', 
+#  'baseType': 'Item Quantity Support', 
+#  'identified': True, 
+#  'ilvl': 0, 
+#  'properties': [{'name': 'Support', 
+#                  'values': [], 
+#                  'displayMode': 0}, 
+#                     {'name': 'Level', 
+#                     'values': [['17', 0]], 
+#                     'displayMode': 0, 
+#                     'type': 5}], 
+#                  'requirements': [{'name': 'Level', 
+#                                    'values': [['64', 0]], 
+#                                    'displayMode': 0, 
+#                                    'type': 62}, 
+#                  {'name': 'Str', 
+#                   'values': [['102', 0]], 
+#                   'displayMode': 1, 
+#                   'type': 63}], 
+#                 'additionalProperties': [{'name': 'Experience', 
+#                                           'values': [['3719002/26185582', 0]], 
+#                                           'displayMode': 2, 
+#                                           'progress': 0.14, 
+#                                           'type': 20}], 
+#                 'secDescrText': 'Supports any skill that can kill enemies.', 
+#                 'explicitMods': ['33% increased Quantity of Items Dropped by Enemies Slain from Supported Skills'], 
+#                 'descrText': 'This is a Support Gem. It does not grant a bonus to your character, but to skills in sockets connected to it. Place into an item socket connected to a socket containing the Active Skill Gem you wish to augment. Right click to remove from a socket.',
+#                 'frameType': 4, 
+#                 'x': 28, 
+#                 'y': 0, 
+#                 'inventoryId': 'Stash1'},
 
-    def get_profile(self):
-        return json.loads(self._get_profile_raw().content)
-
-    def stash_tab_names(self, league="Standard", filter_remove_only=True) -> list:
-        stashes = self.get_stash(league)
-        # scrap stash names
-        temp=[i["name"] for i in stashes["stashes"]]
-        # process names and remove the "remove-only"
-        if filter_remove_only:
-            count = len(temp)
-            for i in range(count):
-                ii = count - i -1
-                if "Remove-only" in temp[ii]:
-                    temp.remove(temp[ii])
-        return temp
-
-    def _parse_names(self, depth=DEPTH_ITEMS, stash_dict=None):
-        """Parse separately so that we don't fetch for every ques """
-        if not stash_dict:
-            stash_dict = self.get_stash()
