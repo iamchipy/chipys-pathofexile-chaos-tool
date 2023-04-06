704646af224b48af60ab2d30da6692c0b6a67357
Chipy <iamchipy@gmail.com>
2023-04-06 13:33:08 -0400

    fixing working dir for BATs

--- cpct/cpct/cpct_download.bat
+++ cpct/cpct/cpct_download.bat
@@ -1,3 +1,3 @@
 pip install cpct --upgrade -t c:/chipy-scripts/
-& "First Install..." "C:/chipy-scripts/cpct/cpct_START_HERE.cmd"
+start "First Install..." "C:/chipy-scripts/cpct/cpct_START_HERE.cmd"
 pause
